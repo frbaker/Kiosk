9c7fcf3721375ebbd382bbbc7e1282eb8c69750e
fbaker@isd381.org <fbaker@isd381.org>
2024-08-21 17:10:52 -0500

    added ideas to home and some cleanup

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,9 +2,81 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <div>
+    <div class="homePageContent">
       <h1>Best of the NorthShore</h1>
       <p>If you have to ask, You've never been there.</p>
+<ul>
+  <li>Welcome Message - brief intro to the community/area</li>
+  <li>Local Announcements / info
+    <ul>
+      <li>School events</li>
+      <li>Community meetings</li>
+      <li>government
+        <ul>
+          <li>Municipal services</li>
+          <li>Contact info</li>
+          <li>Public Meetings</li>
+          <li>Permits and licensing</li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+  <li>Business directory
+    <ul>
+      <li>Job listings</li>
+    </ul>
+  </li>
+  <li>government services</li>
+  <li>Weather conditions</li>
+  <li>Transportation schedule /options
+    <ul>
+      <li>Silver Bay Shuttle Service</li>
+      <li>Cadilac Cab</li>
+    </ul>
+  </li>
+  <li>Events Calendar
+    <ul>
+      <li>Upcoming Events</li>
+      <li>Festivals
+        <ul>
+          <li>Bay Days</li>
+          <li>Music in the Park</li>
+        </ul>
+      </li>
+      <li>Things To Do</li>
+      <li>Dining/Entertainement</li>
+      <li>Accomodations</li>
+      <li>Visitor information
+        <ul>
+          <li>Maps
+            <ul>
+              <li>Save your map / trip planner</li>
+            </ul>
+          </li>
+          <li>Tips</li>
+          <li>Travel Guide</li>
+        </ul>
+      </li>
+      <li>Club meetings/sports leagues</li>
+      <li>Garage sales</li>
+      <li>Event Submission - option for locals to submit events for inclussion</li>
+    </ul>
+  </li>
+  <li>Real Estate</li>
+  <li>Classified
+    <ul>
+      <li>Buy / Sell / Trade</li>
+      <li>Lost and Found</li>
+    </ul>
+  </li>
+  <li>Interactive Maps
+    <ul>
+      <li>Map showing key points of interest</li>
+      <li>Customizable map - scan QR to take with you</li>
+      </ul></li>
+</ul>
+
+<p>Inclussion of these components would create a go-to resource for everything to the local area - fostering community engagement and making the area more accessible to both residents and visitors.</p>
       <ul>
       <li><a href="/kiosk/1">Kiosk 1</a></li>
       <li><a href="/kiosk/2">Kiosk 2</a></li>
--- src/Kiosk.js
+++ src/Kiosk.js
@@ -7,45 +7,54 @@ import "slick-carousel/slick/slick-theme.css";
 
 function Kiosk() {
   const { id } = useParams();
-  const [content, setContent] = useState(null);
+  //const [content, setContent] = useState(null);
   const [loading, setLoading] = useState(true); // Start loading initially
   const [error, setError] = useState(null);
   const [ads, setAds] = useState([]);
-  const [hostName, setHostName] = useState([]);
+  //const [hostName, setHostName] = useState([]);
   const [eventsContent, setEventsContent] = useState(null);
   const [eventsLoading, setEventsLoading] = useState(true); // Set loading for events
   const [eventsError, setEventsError] = useState(null);
-  const [eventsYear, setEventsYear] = useState(null);
+  //const [eventsYear, setEventsYear] = useState(null);
+  const [weatherLoading, setWeatherLoading] = useState(null);
+  const [weatherError, setWeatherError] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     const apiUrl = `http://axoncentral.com/kioskJSON.php?media=player&playerId=${id}`;
     const eventsUrl = `http://bestofthenorthshore.com/events-activities/kioskJSON.php`;
+    const weatherUrl = `https://api.weather.gov/gridpoints/DLH/117,94/forecast`;
 
     // Fetch data from both sources when the component mounts
     const fetchData = async () => {
       try {
-        const [apiResponse, eventsResponse] = await Promise.all([
+        const [apiResponse, eventsResponse, weatherResponse] = await Promise.all([
           axios.get(apiUrl),
-          axios.get(eventsUrl)
+          axios.get(eventsUrl),
+          axios.get(weatherUrl)
         ]);
 
         // Handle the first API response
         const adData = apiResponse.data.data.ads.ads || [];
-        const hostName = apiResponse.data.data.playerName || [];
+        //const hostName = apiResponse.data.data.playerName || [];
         setAds(adData);
-        setHostName(hostName);
-
+        //setHostName(hostName);
         // Handle the second API response (events)
         const eventData = eventsResponse.data.data.events.events || [];
-        const eventsYear = eventsResponse.data.data.events.year || [];
+       //const eventsYear = eventsResponse.data.data.events.year || [];
         setEventsContent(eventData); // Assuming events data is directly available here
-        setEventsYear(eventsYear); // Assuming events data is directly available here
+        //setEventsYear(eventsYear); // Assuming events data is directly available here
+        // Handle the third API response (Weather)
+        const weatherData = weatherResponse.data.properties.periods || [];
+        setWeather(weatherData);
       } catch (err) {
         setError('Under development access not allowed - Unless you are Floyd');
         setEventsError('Failed to fetch events data'); // Handle events error
+        setWeatherError('Failed to fetch Weather');
       } finally {
         setLoading(false); // Stop loading main content
         setEventsLoading(false); // Stop loading events
+        setWeatherLoading(false); // Stop loading weather
       }
     };
 
@@ -84,9 +93,9 @@ function Kiosk() {
   };
 
   return (
-    <div>
-      <div style={{ width: '58%', float: 'left' }}>
-        <h1 style={{ textAlign: 'center', color: 'white' }}>Welcome to {hostName}</h1>
+    <div class="grid-container">
+      <div class="ads">
+        <h1>Welcome to The North Shore {/*hostName*/}</h1>
         {loading && <p>Loading content...</p>} {/* Show loading message */}
         {error && <p>{error}</p>} {/* Show error message */}
         {ads.length > 0 && (
@@ -120,20 +129,45 @@ function Kiosk() {
           </Slider>
         )}
       </div>
-      <div style={{ width: '40%', backgroundColor: 'white', float: 'right' }}>
+      <div class="weather">
+        <div>
+        <h1>Silver Bay Weather</h1>
+        {weatherLoading && <p>Loading weather...</p>} {/* Show loading for weather */}
+        {weatherError && <p>{weatherError}</p>} {/* Show error message for weather */}
+        {weather && (
+          <div className="weather-container flex-container">
+            {weather.map((period, index) => (
+              index < 6 && (
+              <div class="flex">
+                <h3 class="weatherHeader">{period.name}</h3>
+                <p>{period.detailedForecast}</p>
+              </div>
+              )
+            ))}
+          </div>
+        )}
+       
+
+        </div>
+
+      </div>
+      <div class="events">
         <h3 style={{ textAlign: 'center' }}>Best Of the North Shore</h3>
-        <h4 style={{ textAlign: 'center' }}>{eventsYear} Events Calendar</h4>
+        <h4 class="eventCal">Area Events</h4>
+        <p class="eventSwipe">Swipe to navigate</p>
         {eventsLoading && <p>Loading events...</p>} {/* Show loading for events */}
         {eventsError && <p>{eventsError}</p>} {/* Show error message for events */}
         {eventsContent && (
-          <div className="slider-container">
+          <div className="slider-container events-slide">
             <Slider {...eventSliderSettings}>
               {eventsContent.map((event, index) => (
                 <span key={index} style={{backgroundColor:'grey', border:'solid 1px black'}}>
-                  <p>{event.location}<br />{event.startDate} - {event.enddate}</p>
+                  <p><span class="eventDate">{event.startDate} {event.endDate}</span><br />
+                    <span class="eventTime">{event.startTime} - {event.endTime}</span><br />
+                    {event.location}</p>
                   <h3>{event.title}</h3>
                   <span dangerouslySetInnerHTML={{ __html: event.description }} />
-                  <hr />
+                  <br /><br />
                 </span>
               ))}
             </Slider>
